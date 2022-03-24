8125282200238c75b0a13214cc8c0862aa36709b
IMRANDIL <aliimranadil2@gmail.com>
2022-03-24 06:01:48 +0530

    yh

--- src/components/AddExpenseForm.jsx
+++ src/components/AddExpenseForm.jsx
@@ -1,23 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+
 
 
 
 
 
 const AddExpenseForm = () => {
+
+    const [name, setName] = useState('');
+
+    const [cost, setCost] = useState('')
+
+
+
+
+    const handlesubmit = (e) => {
+        e.preventDefault();
+
+        alert(`name ${name}, cost ${cost}`)
+    }
+
+
+
+
+
+
+
+
     return (
-        <form>
-            <div className="row">
+        <form onSubmit={handlesubmit}>
+            <div className="row ">
                 <div className="col-sm">
                     <label htmlFor="name">Name</label>
-                    <input type='text' required placeholder='Name' className='form-control' id='name' />
+                    <input type='text' required placeholder='Name' className='form-control' id='name' value={name} onChange={(e) => setName(e.target.value)} />
                 </div>
                 <div className="col-sm">
                     <label htmlFor="cost">Cost</label>
-                    <input type="text" required placeholder='Cost' className='form-control' id='cost' />
+                    <input type="text" required placeholder='Cost' className='form-control' id='cost' value={cost} onChange={(e) => setCost(e.target.value)} />
                 </div>
                 <div className="col-sm">
-                    <button type='submit' className='btn btn-primary'>Save</button>
+                    <button type='submit' className='btn btn-primary mt-3'>Save</button>
                 </div>
             </div>
         </form>
--- src/components/ExpenseList.jsx
+++ src/components/ExpenseList.jsx
@@ -1,21 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import ExpenseItem from './ExpenseItem'
 
-
+import { AppContext } from '../context/AppContext'
 
 
 
 
 const ExpenseList = () => {
 
-    const expenses = [
-        { id: 1444, name: 'Shopping', cost: 540 },
-        { id: 144, name: 'Holiday', cost: 507 },
-        { id: 1044, name: 'Transportation', cost: 590 },
-        { id: 1484, name: 'Fuel', cost: 508 },
-        { id: 1474, name: 'Child care', cost: 550 },
-        { id: 1446, name: 'Love', cost: 500 },
-    ]
+
+    const { expenses } = useContext(AppContext)
 
 
 
